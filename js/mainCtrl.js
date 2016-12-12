afcee2cbef0033aff4b5194f50d4a6d33b5da16f
Bryan Jackson <bjack13@gmail.com>
2016-12-12 12:41:30 -0700

    finished mini-project

--- js/mainCtrl.js
+++ js/mainCtrl.js
@@ -1,6 +1,11 @@
 angular.module('friendsList')
 .controller('mainCtrl',function ($scope) {
 
-  $scope.friends = ['Scott',  'tom', 'Doug', 'dog','batman',];
+  $scope.friends = ['Scott',  'tom', 'Doug', 'dog','batman'];
+
+  $scope.addFriend = function (addName) {
+    $scope.friends.push(addName);
+    console.log($scope.friends);
+  }
 
 });
